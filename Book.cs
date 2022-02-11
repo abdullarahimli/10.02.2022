0c257e123908539c146e7b89ae7dc91d0dcddf98
abdullarahimli <abdullavr@code.edu.az>
2022-02-11 04:51:05 -0800

    First Commit

---
+++ Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class Book
+    {
+        public string Name;
+        public string Author;
+        public int PageCount;
+
+       
+        public Book (string name, int pageCount)
+        {
+            Name = name;
+            PageCount = pageCount;
+
+            if (pageCount <= 10)
+            {
+                Console.WriteLine("Sehife azdir");
+            }
+            else if (name.Length <= 3)
+            {
+                Console.WriteLine("Duzgun daxil et");
+            }
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kitabi daxil edin");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Ikinci Kitabi daxil edin");
+            string Name1 = Console.ReadLine();
+            Console.WriteLine("Sehifeni daxil edin");
+            int PageCount = int.Parse(Console.ReadLine());
+            
+            string[] arr = new string[10];
+           
+            Book book1 = new Book(Name, PageCount);
+
+            //book1.Name = "";
+            //book1.Author = "";
+            //book1.PageCount = 2;
+
+            //Book book2 = new Book("Psikoloji", 650);
+
+            //book2.Name = "Psikoloji";
+            //book2.Author = "Anonyms";
+            //book2.PageCount = 650;
+
+            Console.WriteLine(book1.Name);
+            Console.WriteLine(book1.PageCount);
+        }
+    }
+}
